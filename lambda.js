d9657c6ba4c175c23d41a02a84e6566d4934d3e8
maysakanoni <maysa@madetech.com>
2022-04-05 11:01:16 +0100

    add serverless and lambda.js files

---
+++ lambda.js
@@ -0,0 +1,16 @@
+const server = require('restana')();
+const app = require('next')({ dev: false });
+const files = require('serve-static');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nextRequestHandler = app.getRequestHandler();
+
+server.use(files(path.join(__dirname, 'build')));
+
+server.all('/api/*', bodyParser.json(), bodyParser.urlencoded(), (req, res) =>
+  nextRequestHandler(req, res)
+);
+
+server.all('*', (req, res) => nextRequestHandler(req, res));
+
+module.exports.handler = require('serverless-http')(server);
