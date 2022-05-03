ad0eb52c01c68709dd98f0d5ca003590df565b66
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-05-03 09:37:53 +0100

    Correctly exported global constants

--- globals.js
+++ globals.js
@@ -1,4 +1,2 @@
-const customerServicesTelephoneNumber = '01522 873333'
-const outOfHoursCustomerServicesTelephoneNumber = '01522 534747'
-
-export default {customerServicesTelephoneNumber, outOfHoursCustomerServicesTelephoneNumber}
+export const customerServicesTelephoneNumber = '01522 873333'
+export const outOfHoursCustomerServicesTelephoneNumber = '01522 534747'
