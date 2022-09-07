578f91f4a1b6c81bdea91c46d8bf8eb36b4c9191
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-09-07 12:40:21 +0100

    rm commented out code

--- pages/api/maintenance/index.js
+++ pages/api/maintenance/index.js
@@ -5,9 +5,6 @@ const { checkMaintenanceModeGateway, sentryParams } = require('../gateways');
 export default async function (req, res) {
   Sentry.init(sentryParams);
 
-  // res.status(200).json({ maintenanceModeEnabled: true });
-  // return;
-
   try {
     let result = await checkMaintenanceModeGateway(req.body);
     res.status(200).json(result)
