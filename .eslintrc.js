f9902c7ca635a67308978bc15cf2ca9eea04198b
ange47rm <ange47rm@hotmail.com>
2022-12-14 15:03:05 +0000

    Changed eslintrc.js back to what it was

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
   'rules': {
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
-    'quotes': ['error', 'single'],
-    'linebreak-style': ["warning", "windows"]
+    'quotes': ['error', 'single']
   }
 };
