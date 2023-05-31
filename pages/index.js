ed827fde230deb1a36cb618adf08abae33e28b21
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-05-31 12:21:06 +0100

    Content changes (#71)
    * Content changes
    
    * Update tests

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,55 @@
 import WarningText from '../compoments/warningText';
-import Details from '../compoments/details';
-import TextLink from '../compoments/textLink';
 import React from 'react';
-import Link from 'next/link'
-import ContactNumbers from '../compoments/contactNumbers';
-import OpeningHours from '../compoments/report-repair/opening-hours';
-import {customerServicesTelephoneNumber} from '../globals';
+import Link from 'next/link';
 
 export default function Home() {
-
   return (
     <div className="govuk-grid-row govuk-body-m govuk-!-margin-top-7 lbh-body-m">
       <div className="govuk-grid-column-two-thirds">
         <h1 className={'govuk-heading-xl lbh-heading-h1'}>Request a repair</h1>
         <h2 className={'lbh-heading-h2'}>Before you start</h2>
+
         <p className={'lbh-body-m'}>
-        To request a repair for a <strong>communal area</strong>, please call customer services <strong>{customerServicesTelephoneNumber}</strong>.
+          Only council tenants can request repairs online. If you’re a council
+          tenant, you can request:
         </p>
-        <WarningText  testid="landing-page-report-limit-warning" className="govuk-!-margin-top-4 lbh-body lbh-!-font-weight-bold">
-          This service will allow you to raise all non-emergency repairs within your own home.
-        </WarningText>
-        <WarningText  testid="landing-page-report-limit-warning" className="govuk-!-margin-top-4 lbh-body lbh-!-font-weight-bold">
-          This service can only be used to request one repair at a time to a council property.
-        </WarningText>
+
+        <br />
+
+        <ul className={'govuk-list govuk-list--bullet lbh-list--bullet'}>
+          <li>non-emergency repairs</li>
+          <li>internal repairs</li>
+        </ul>
+
         <p className={'lbh-body-m'}>
-          If you <strong>own the property</strong>, please see our <TextLink href="https://hackney.gov.uk/leaseholders-and-freeholders" target={'_blank'}>leaseholder contact details page</TextLink>.
+          If you own the property, you cannot request a repair online - call{' '}
+          <strong style={{ whiteSpace: 'nowrap' }}>020 8356 3691</strong>.
         </p>
-        <p className={'lbh-body-m'}>
-        To request a repair for a <strong>communal area</strong>, please call customer services <strong>{customerServicesTelephoneNumber}</strong>.
-        </p>
-        <Details summary="What is a communal area?" className="govuk-!-margin-top-6" data-testid="landing-page-communal-prompt">
-          <div data-testid='landing-page-communal-info'>
-            <p>Communal repairs are usually in areas that people share.</p>
-            <p>They can include:</p>
-            <ul className={'govuk-list govuk-list--bullet lbh-list--bullet'}>
-              <li>repairs to door entry systems</li>
-              <li>lock repairs to communal doors</li>
-              <li>lighting repairs to shared areas</li>
-              <li>glazing repairs to shared doors or stairway windows</li>
-              <li>roof and gutter repairs</li>
-              <li>structure and external walls to your block</li>
-            </ul>
-          </div>
-        </Details>
-        <WarningText testid="landing-page-gas-warning" className="lbh-body lbh-!-font-weight-bold">
-          If you suspect you have a gas leak, you must report it immediately
-          to the Gas Emergency Service on 0800 111 999 or via textphone
-          (minicom) on 0800 371 787
+
+        <WarningText
+          testid="landing-page-report-limit-warning"
+          className="govuk-!-margin-top-4 lbh-body lbh-!-font-weight-bold"
+        >
+          This service can only be used to request one repair at a time.
         </WarningText>
-        <WarningText testid="landing-page-emergency-warning" className="lbh-body lbh-!-font-weight-bold">
-          For other emergency repairs, please call customer services <strong>{customerServicesTelephoneNumber}</strong>.
-        </WarningText>
-        <Details summary="What is an emergency?" className="govuk-!-margin-top-6" testid="landing-page-emergency-prompt">
-          <div data-testid='landing-page-emergency-info'>
-            An emergency is defined as something which could cause danger to
-            someone’s health or safety or cause serious damage and destruction
-            to property.
-            <ContactNumbers/>
-            <p>
-              Opening times:
-            </p>
-            <OpeningHours/>
-          </div>
-          <div>
-            For other emergency repairs such as no water, leaks, no heating or hot water, blocked drain please call customer services 0208 356 3691
-          </div>
-        </Details>
+
         <Link href="/report-repair/priority-list">
-          <a role="button" draggable="false"
+          <a
+            role="button"
+            draggable="false"
             className="govuk-button lbh-button govuk-button--start"
-            data-module="govuk-button">
-              Start now
-            <svg className="govuk-button__start-icon" xmlns="http://www.w3.org/2000/svg" width="17.5" height="19" viewBox="0 0 33 40" aria-hidden="true" focusable="false">
+            data-module="govuk-button"
+          >
+            Start now
+            <svg
+              className="govuk-button__start-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="17.5"
+              height="19"
+              viewBox="0 0 33 40"
+              aria-hidden="true"
+              focusable="false"
+            >
               <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
             </svg>
           </a>
--- tests/cypress/integration/landingPage.spec.js
+++ tests/cypress/integration/landingPage.spec.js
@@ -13,38 +13,6 @@ describe('App', () => {
     cy.contains('This is our new website design - it\'s work in progress');
   });
 
-  it('displays a smell gas warning on the landing page', () => {
-    cy.get('[data-testid=landing-page-gas-warning]').should(
-      'have.contain',
-      'If you suspect you have a gas leak, you must report it immediately to the Gas Emergency Service on 0800 111 999 or via textphone (minicom) on 0800 371 787'
-    );
-  });
-
-  it('displays a emergency repair warning on the landing page', () => {
-    cy.get('[data-testid=landing-page-emergency-warning]').should(
-      'have.contain',
-      'For other emergency repairs, please call customer services'
-    );
-  });
-
-  context('emergency prompt', () => {
-    it('displays text', () => {
-      cy.get('[data-testid=landing-page-emergency-prompt]').should(
-        'have.contain',
-        'What is an emergency?'
-      );
-    });
-
-    it('displays instructions when clicked', () => {
-      cy.get('[data-testid=landing-page-emergency-prompt] summary')
-        .click()
-        .then(() => {
-          cy.get('[data-testid=landing-page-emergency-info]')
-            .should('be.visible')
-        });
-    });
-  });
-
   it('displays a start button', () => {
     cy.get('a').contains('Start now').should('have.attr', 'href', '/report-repair/priority-list');
   });
