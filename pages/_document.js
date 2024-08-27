12ea3159e805a456a6346b29fbecdcc08f2695c8
Adam <adam.tracy20@gmail.com>
2024-08-27 13:22:21 +0100

    Fix

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,6 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
-  gtmTokenId = process.env.NEXT_PUBLIC_GTM_TOKEN_ID || '';
-
   render() {
     return (
       <Html>
@@ -20,10 +18,10 @@ class MyDocument extends Document {
         <body className={'js-enabled lbh-body'}>
           <Main />
           <NextScript />
-          {gtmTokenId && (
+          {process.env.NEXT_PUBLIC_GTM_TOKEN_ID && (
             <noscript
               dangerouslySetInnerHTML={{
-                __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmTokenId}" height="0" width="0" style="display: none; visibility: hidden;" />',
+                __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_TOKEN_ID}" height="0" width="0" style="display: none; visibility: hidden;" />',
               }}
             />
           )}
