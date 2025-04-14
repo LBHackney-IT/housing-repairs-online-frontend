6216367ef732a5c4623210cfceea5da98c8c9e8e
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-04-14 11:44:35 +0100

    add logging

--- lambda.js
+++ lambda.js
@@ -3,19 +3,61 @@ const path = require('path');
 const files = require('serve-static');
 const bodyParser = require('body-parser');
 
-process.env.NODE_ENV = 'production';
+console.log('Lambda cold start - initializing server');
 
-server.use('/_next/static', files(path.join(__dirname, 'build/_next/static')));
-server.use(files(path.join(__dirname, 'public')));
+try {
+  console.log('Loading Next.js standalone server');
+  const app = require('./build/_next/standalone/server');
+  const nextRequestHandler = app.getRequestHandler();
+  console.log('Next.js standalone server loaded successfully');
 
-const nextApp = require('./build/_next/standalone/server');
-const nextRequestHandler = nextApp.getRequestHandler();
+  server.use(files(path.join(__dirname, 'build/_next/static'), {
+    index: false,
+    maxAge: '30d',
+    prefix: '/_next/static'
+  }));
+  server.use(files(path.join(__dirname, 'public')));
 
-server.all('/api/*', bodyParser.json(), bodyParser.urlencoded({ extended: true}), (req, res) =>
-  nextRequestHandler(req, res)
-);
+  server.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
 
-server.all('', (req, res) => nextRequestHandler(req, res));
-server.all('*', (req, res) => nextRequestHandler(req, res));
+    const originalEnd = res.end;
+    res.end = function () {
+      console.log(`Response sent: ${req.method} ${req.url} - Status: ${res.statusCode}`);
+      return originalEnd.apply(this, arguments);
+    };
+
+    next();
+  });
+
+  server.all('/api/*', bodyParser.json(), bodyParser.urlencoded({ extended: true }), (req, res) => {
+    console.log('Handling API request');
+    nextRequestHandler(req, res);
+  });
+
+  server.all('', (req, res) => {
+    console.log('Handling root request');
+    nextRequestHandler(req, res);
+  });
+  server.all('*', (req, res) => {
+    console.log(`Handling wildcard request: ${req.url}`);
+    nextRequestHandler(req, res);
+  });
+
+} catch (error) {
+  console.error('Error during server initialization:', error);
+
+  server.all('*', (req, res) => {
+    console.error('Request received but server failed to initialize');
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: 'Server initialization failed', details: error.message }));
+  });
+}
+
+server.use((err, req, res, next) => {
+  console.error(`Server error for ${req.method} ${req.url}:`, err);
+  res.statusCode = 500;
+  res.end(JSON.stringify({ error: 'Internal Server Error', details: err.message }));
+});
 
 module.exports.handler = require('serverless-http')(server);
