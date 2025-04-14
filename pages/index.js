fdc7b82d3368ef57cfb376429896eb450b0e18b1
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-04-14 08:46:09 +0100

    Upgrade to next 13 (#93)
    * Upgrade packages
    
    * fix legacy links

--- pages/index.js
+++ pages/index.js
@@ -36,26 +36,25 @@ export default function Home() {
           </div>
         </Details>
 
-        <Link href="/report-repair/priority-list">
-          <a
-            role="button"
-            draggable="false"
-            className="govuk-button lbh-button govuk-button--start"
-            data-module="govuk-button"
+        <Link
+          href="/report-repair/priority-list"
+          role="button"
+          draggable="false"
+          className="govuk-button lbh-button govuk-button--start"
+          data-module="govuk-button">
+          Start now
+                      <svg
+            className="govuk-button__start-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="17.5"
+            height="19"
+            viewBox="0 0 33 40"
+            aria-hidden="true"
+            focusable="false"
           >
-            Start now
-            <svg
-              className="govuk-button__start-icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="17.5"
-              height="19"
-              viewBox="0 0 33 40"
-              aria-hidden="true"
-              focusable="false"
-            >
-              <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
-            </svg>
-          </a>
+            <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+          </svg>
+
         </Link>
       </div>
     </div>
