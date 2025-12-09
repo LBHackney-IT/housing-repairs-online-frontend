7fb6ec3a4a88b7a98616772a71879069df0c318d
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-12-09 09:38:39 +0000

    Cleanup

--- lambda.js
+++ lambda.js
@@ -3,7 +3,6 @@ const files = require('serve-static');
 const path = require('path');
 
 const standaloneDir = path.join(__dirname, 'build/_next/standalone');
-// const standaloneDir = path.join(__dirname, '../build/_next/standalone');
 const Next = require(path.join(standaloneDir, 'node_modules/next'));
 
 // https://github.com/vercel/next.js/issues/64031#issuecomment-2078708340
@@ -30,8 +29,6 @@ async function init() {
   }
 }
 
-// server.use(files(path.join(__dirname, '../build')));
-// server.use(files(path.join(__dirname, '../public')));
 server.use(files(path.join(__dirname, 'build')));
 server.use(files(path.join(__dirname, 'public')));
 
@@ -41,25 +38,3 @@ server.all('*', async (req, res) => {
 });
 
 module.exports.handler = require('serverless-http')(server);
-
-// const server = require('restana')()
-// const app = require('next')({ dev: false })
-// const files = require('serve-static')
-// const path = require('path')
-
-// const getHandler = async () => {
-//   await app.prepare()
-//   const nextRequestHandler = app.getRequestHandler()
-
-//   server.use(files(path.join(__dirname, 'build')))
-//   server.use(files(path.join(__dirname, 'public')))
-
-//   server.all('*', (req, res) => nextRequestHandler(req, res))
-
-//   return require('serverless-http')(server)
-// }
-
-// module.exports.handler = async (event, context) => {
-//   const serverlessHandler = await getHandler()
-//   return serverlessHandler(event, context)
-// }
